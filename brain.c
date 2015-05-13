c58dfa3d308a5f5625bbc0dcbdb1da04085a6f9e
toastedcornflakes <toastedcornflakes@gmail.com>
2015-05-13 13:27:06 +0200

    Rewrite for use with FILE* instead of raw I/O; supports stdin

--- brain.c
+++ brain.c
@@ -15,10 +15,10 @@ int main(int argc, char **argv) {
 	unsigned char *command_file = NULL;
 	int use_interpreter = 0;
 
-	int fd = -1;
+	FILE *script_file = NULL;
 	char *script_path = argv[argc - 1];
 
-	check(argc == 2 || argc == 3, "Usage: %s [--interpreter|--JIT] script", argv[0]);
+	check(argc == 2 || argc == 3, "Usage: %s [--interpreter|--JIT] [script|-]", argv[0]);
 	
 	if(strcmp(argv[1], "--interpreter") == 0) {
 		debug("Using interpreter");
@@ -27,25 +27,28 @@ int main(int argc, char **argv) {
 		debug("Using JIT compiler");
 	}
 
-	fd = open(script_path, O_RDWR, 0);
-	check(fd != -1, "Couldn't open script %s", script_path);
+	if(strcmp(script_path, "-") == 0) {
+		script_file = stdin;
+	} else {
+		script_file = fopen(script_path, "r");
+	}
+	check(script_file != NULL, "Couldn't open script %s", script_path);
 
 	// get size of brainfuck instructions file
-	struct stat st;
-	check(fstat(fd, &st) == 0, "stat");
-	size_t command_size = st.st_size;
+	fseek(script_file, 0L, SEEK_END);
+	size_t command_size = ftell(script_file);
+	fseek(script_file, 0L, SEEK_SET);
+
 
 	debug("command_size: %zu", command_size);
-	debug("File descriptor: %d", fd);
 
-	// mmap the brainfuck instructions file to hand it to the interpreter/compiler
-	command_file = mmap(NULL, command_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
-	close(fd);
-	if (command_file == MAP_FAILED) {
-		log_err("MAP_FAILED, exiting");
-		exit(-1);
-	}
-	debug("mmaped %s at %p", script_path, command_file);
+	// read instructions file to hand it to the interpreter/compiler
+	command_file = malloc(command_size);
+	check_mem(command_file);
+	
+	size_t read_bytes = fread(command_file, 1, command_size, script_file);
+	check(read_bytes == command_size, "fread() failed to read the script");
+	fclose(script_file);
 
 	if(use_interpreter) {
 		interpreter_main_loop(command_file, command_file + command_size);
@@ -56,7 +59,7 @@ int main(int argc, char **argv) {
 
 error:
 	if(command_file) {
-		munmap(command_file, command_size);
+		free(command_file);
 	}
 }
 
