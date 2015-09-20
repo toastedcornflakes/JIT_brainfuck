a77779723ff26c5d5a0b49cda9d0a810cc71d9d2
toastedcornflakes <toastedcornflakes@gmail.com>
2015-09-20 13:22:15 +0200

    Explain better why we don't put a NUL for printing

--- asm/print_linux.asm
+++ asm/print_linux.asm
@@ -2,7 +2,7 @@ BITS 64
 
 mov rax, 1	    ; write is syscall number 1
 mov rdi, 1      ; write to stdout
-mov rsi, rdx    ; rdx is the address of the one character string [rdx] (forgetting the NUL byte works)
+mov rsi, rdx    ; rdx is the address of the one character string [rdx] (doesn't need a NUL byte since we specify the length of the message)
 push rdx        ; save rdx to stack
 mov rdx, 1      ; size of message
 syscall			; yay write
--- asm/print_osx.asm
+++ asm/print_osx.asm
@@ -2,7 +2,7 @@ BITS 64
 
 mov rax, 0x200018d	 ; write_nocancel is used by libc
 mov rdi, 1           ; to stdout
-mov rsi, rdx         ; rdx is the address of the one character string [rdx] (forgetting the NUL byte)
+mov rsi, rdx         ; rdx is the address of the one character string [rdx] (doesn't need a NUL byte since we specify the length of the message)
 push rdx             ; save rdx to stack
 mov rdx, 1           ; size of message
 syscall             
